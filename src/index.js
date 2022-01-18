c94f108386f4342799c1a4c1f45b4776638eab9c
Dyego Maas <dyego.maas@gmail.com>
2022-01-18 07:49:07 -0300

    Ajuste.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(`mongodb://mongodb:27017/test?connectTimeoutMS=${process.env.CONNECTION_TIMEOUT_MS || 30000}`)
+  .connect(`mongodb://mongodb:27017?connectTimeoutMS=${process.env.CONNECTION_TIMEOUT_MS || 30000}`)
   .then(() => {
     console.log('conectou')
   }, error => console.log(error));
